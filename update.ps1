d81cbc2f7cc53e8bd72382bd5d1b565bd617aad2
Larry H. <111525408+larryhuangdev@users.noreply.github.com>
2024-08-08 01:23:11 +0700

    Initial commit

---
+++ update.ps1
@@ -0,0 +1,38 @@
+#Requires -RunAsAdministrator
+
+# Define variables
+$APPDATA = [System.Environment]::GetFolderPath('ApplicationData')
+$DOTDIR = (Get-Location).Path
+
+# Function to set symbolic link
+function Set-Symlink {
+    param (
+        [string]$Path,
+        [string]$Target
+    )
+
+    if (Test-Path $Path) {
+        # Check if the existing item is a symbolic link
+        $item = Get-Item $Path
+        if ($item.Attributes -band [System.IO.FileAttributes]::ReparsePoint) {
+            Write-Output "Removing the existing symlink $Path"
+            Remove-Item $Path
+        }
+        else {
+            # Get the current epoch timestamp
+            $epochTime = [int][double]::Parse((Get-Date -UFormat %s))
+            # Generate the new name with .old and epoch timestamp
+            $newName = "$Path.old.$epochTime"
+            # Rename the existing file or directory
+            Write-Output "Conflict detected: $Path already exists. Renaming to $newName"
+            Rename-Item -Path $Path -NewName $newName
+        }
+    }
+
+    # Set the symbolic link
+    New-Item -ItemType SymbolicLink -Path $Path -Target $Target
+}
+
+# Link configs
+Set-Symlink -Path "$APPDATA\Code\User\keybindings.json" -Target "$DOTDIR\vscode\keybindings.json"
+Set-Symlink -Path "$HOME\.gitconfig" -Target "$DOTDIR\git\.gitconfig"
